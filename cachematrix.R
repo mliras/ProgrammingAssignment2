dc60772f5b3194fa230607f444d6856a8eb16824
mliras <martin.liras@gmail.com>
2014-05-24 01:31:35 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        s <- NULL
+	
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+		s <<- NULL
+        }
 
+        get <- function() x
+
+        setmean <- function(mean) m <<- mean
+
+        getmean <- function() m
+
+        setsolve <- function(solve) s <<- solve
+
+        getsolve <- function() s
+	
+
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean,
+	     setsolve = setsolve,
+	     getsolve = getsolve)
 }
 
 
@@ -12,4 +36,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        		i<- x$getsolve()
+		
+		if (is.null(i))
+		{
+			y<-x$get()
+			i<<-solve(y)
+			x$setsolve(i)					
+			i
+		}	
+		else
+		{
+			return(i)
+		}
 }
